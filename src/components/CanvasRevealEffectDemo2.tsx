9977b7eddbe72cf602181a759ecf82caa80913e5
paulp111 <133204904+paulp111@users.noreply.github.com>
2024-07-26 11:45:54 +0200

    save

--- src/components/CanvasRevealEffectDemo2.tsx
+++ src/components/CanvasRevealEffectDemo2.tsx
@@ -11,35 +11,35 @@ export function CanvasRevealEffectDemo2() {
     router.push('/tracing-beam-demo');
   };
 
-return (
+  return (
     <div className="w-screen h-screen overflow-hidden relative">
-        <AnimatePresence>
-            <div className="h-full w-full absolute inset-0">
-                <CanvasRevealEffect
-                    animationSpeed={3}
-                    containerClassName="bg-black"
-                    colors={[
-                        [236, 72, 153],
-                        [232, 121, 249],
-                    ]}
-                    dotSize={2}
-                />
-            </div>
-        </AnimatePresence>
-        <div className="relative z-20 flex items-center justify-center h-full flex-col">
-            <h2 className="text-white text-3xl font-bold mb-8">
-                With insomnia, nothing&apos;s real. Everything is far away.
-            </h2>
-            <h2 className="text-white text-3xl font-bold mb-8">
-                Everything is a copy, of a copy, of a copy
-            </h2>
-            <button
-                onClick={handleClick}
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
-            >
-                Go to Main Page
-            </button>
+      <AnimatePresence>
+        <div className="h-full w-full absolute inset-0">
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-gray-900"
+            colors={[
+              [236, 72, 153],
+              [232, 121, 249],
+            ]}
+            dotSize={2}
+          />
         </div>
+      </AnimatePresence>
+      <div className="relative z-20 flex items-center justify-center h-full flex-col">
+        <h2 className="text-white text-3xl font-bold mb-8">
+          With insomnia, nothing&apos;s real. Everything is far away.
+        </h2>
+        <h2 className="text-white text-3xl font-bold mb-8">
+          Everything is a copy, of a copy, of a copy
+        </h2>
+        <button className="p-[3px] relative" onClick={handleClick}>
+          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+          <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
+            Lit up borders
+          </div>
+        </button>
+      </div>
     </div>
-);
+  );
 }
--- src/components/TracingBeamDemo.tsx
+++ src/components/TracingBeamDemo.tsx
@@ -3,39 +3,37 @@ import React from "react";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "@/components/ui/TracingBeam";
-import { BackgroundBoxesDemo } from "@/components/BackgroundBoxesDemo";
 
 export function TracingBeamDemo() {
   return (
-    <TracingBeam className="relative">
-      <div className="max-w-2xl mx-auto antialiased pt-4 relative">
-        <BackgroundBoxesDemo />
-        {dummyContent.map((item, index) => (
-          <div key={`content-${index}`} className="mb-10">
-            <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
-              {item.badge}
-            </h2>
+    <div className="min-h-screen bg-gray-900 text-white">
+      <TracingBeam className="relative">
+        <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+          {dummyContent.map((item, index) => (
+            <div key={`content-${index}`} className="mb-10">
+              <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
+                {item.badge}
+              </h2>
 
-            <p className={twMerge("text-xl mb-4")}>
-              {item.title}
-            </p>
+              <p className={twMerge("text-xl mb-4")}>{item.title}</p>
 
-            <div className="text-sm prose prose-sm dark:prose-invert">
-              {item?.image && (
-                <Image
-                  src={item.image}
-                  alt="blog thumbnail"
-                  height="1000"
-                  width="1000"
-                  className="rounded-lg mb-10 object-cover"
-                />
-              )}
-              {item.description}
+              <div className="text-sm prose prose-sm dark:prose-invert">
+                {item?.image && (
+                  <Image
+                    src={item.image}
+                    alt="blog thumbnail"
+                    height="1000"
+                    width="1000"
+                    className="rounded-lg mb-10 object-cover"
+                  />
+                )}
+                {item.description}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </TracingBeam>
+          ))}
+        </div>
+      </TracingBeam>
+    </div>
   );
 }
 
